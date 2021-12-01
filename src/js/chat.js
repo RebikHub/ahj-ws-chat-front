1a71168686f4b6230a85c02f4a430bb91727069f
Alexandr Rebikov <rebikovlk@mail.ru>
2021-12-01 17:33:56 +0700

    textarea

--- src/js/chat.js
+++ src/js/chat.js
@@ -1,5 +1,65 @@
 export default class Chat {
   constructor(server) {
     this.server = server;
+    this.container = document.querySelector('#container');
+    this.nickname = document.querySelector('.nickname');
+    this.chat = document.querySelector('.chat');
+    this.inputNickname = document.querySelector('.input-nickname');
+    this.btnNickname = document.querySelector('.btn-nickname');
+    this.nicknamesInChat = document.querySelector('.nicknames-in-chat');
+    this.inputMessage = document.querySelector('.input-chat-message');
+    this.chatMessage = document.querySelector('.chat-message');
+    this.inputNameText = null;
+  }
+
+  events() {
+    this.inputName();
+    this.btnNameClick();
+    this.inputTextMessage();
+  }
+
+  inputName() {
+    this.inputNickname.addEventListener('input', (ev) => {
+      this.inputNameText = ev.target.value;
+    });
+  }
+
+  inputTextMessage() {
+    this.inputMessage.addEventListener('input', (ev) => {
+      const span = document.createElement('span');
+      span.textContent = ev.target.value;
+      this.chatMessage.appendChild(span);
+    });
+  }
+
+  async loadUsers(nickname) {
+    if (nickname !== null) {
+      const users = await this.server.load();
+      if (!users.includes(nickname)) {
+        this.server.add(nickname);
+        users.push(nickname);
+      }
+      this.renderChat(users);
+    }
+  }
+
+  renderChat(users) {
+    this.nickname.classList.add('none');
+    this.chat.classList.remove('none');
+    users.forEach((elem) => {
+      const span = document.createElement('span');
+      const img = document.createElement('img');
+      const p = document.createElement('p');
+      p.textContent = elem;
+      span.appendChild(img);
+      span.appendChild(p);
+      this.nicknamesInChat.appendChild(span);
+    });
+  }
+
+  btnNameClick() {
+    this.btnNickname.addEventListener('click', () => {
+      this.loadUsers(this.inputNameText);
+    });
   }
 }
--- src/js/server.js
+++ src/js/server.js
@@ -1,5 +1,27 @@
 export default class Server {
   constructor() {
-    this.url = 'http://locahost:3333';
+    this.url = 'http://localhost:3333/users';
+    this.contentTypeHeader = { 'Content-Type': 'application/json' };
+  }
+
+  async add(nickname) {
+    const response = await fetch(this.url, {
+      body: JSON.stringify(nickname),
+      method: 'POST',
+      headers: this.contentTypeHeader,
+    });
+    const result = await response.json();
+    return console.log(`Response server: ${result}`);
+  }
+
+  async load() {
+    const response = await fetch(this.url);
+    const result = await response.json();
+    console.log(`Response server: ${result}`);
+    return result;
+  }
+
+  remove(id) {
+    return fetch(`${this.url}/${id}`, { method: 'DELETE' });
   }
 }
