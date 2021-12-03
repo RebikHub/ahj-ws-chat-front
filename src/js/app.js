e3b64dd9c637b4bef4e172254032f37bcfd87043
Alexandr Rebikov <rebikovlk@mail.ru>
2021-12-03 15:22:38 +0700

    http

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@ import Server from './server';
 
 console.log('app started');
 
-const ws = new WebSocket('ws://https://ahj-ws-chat-server.herokuapp.com/ws');
+const ws = new WebSocket('ws://ahj-ws-chat-server.herokuapp.com/ws');
 const server = new Server();
 const conteiner = new Chat(server, ws);
 
